ec688f46d3acb86b9ef930dfbe7e573fdbb62488
fiveraptor <jorisbieg@gmail.com>
2024-09-05 13:58:31 +0200

    added delete button

--- web-server/app.js
+++ web-server/app.js
@@ -22,29 +22,26 @@ db.connect((err) => {
   // EJS als Template-Engine einstellen
   app.set('view engine', 'ejs');
   
-  // Funktion zur Aktualisierung des Online-Status, basierend auf der Zeit, wann der Client zuletzt online war
-  function updateClientStatus(callback) {
-    // Setzen wir den Wert auf 10 Sekunden, da das Client-Skript alle 5 Sekunden aktualisiert wird
-    let sql = `
-      UPDATE clients
-      SET online_status = 0
-      WHERE TIMESTAMPDIFF(SECOND, last_online, NOW()) > 10;
-    `;
+  // Route zum Löschen eines Clients
+  app.get('/delete-client/:client_id', (req, res) => {
+    const clientId = req.params.client_id;
+  
+    // Lösche den Client und alle zugehörigen Informationen (Cascading Delete)
+    let sql = `DELETE FROM clients WHERE id = ?`;
     
-    db.query(sql, (err, result) => {
+    db.query(sql, [clientId], (err, result) => {
       if (err) throw err;
-      callback();
+      console.log(`Client mit der ID ${clientId} erfolgreich gelöscht`);
+      res.redirect('/'); // Zur Hauptübersicht zurückkehren
     });
-  }
+  });
   
   // Startseite: Liste aller Clients anzeigen
   app.get('/', (req, res) => {
-    updateClientStatus(() => {
-      let sql = 'SELECT * FROM clients';
-      db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.render('index', { clients: result });
-      });
+    let sql = 'SELECT * FROM clients';
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      res.render('index', { clients: result });
     });
   });
   
